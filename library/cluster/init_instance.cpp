14d20404d6f0fa3d94f0455e101ca7aab164c11f
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2013-06-10 19:29:50 -0400

    libcluster/init_instance.cpp: fix redeclaration of 'i' error

--- library/cluster/init_instance.cpp
+++ library/cluster/init_instance.cpp
@@ -217,9 +217,9 @@ void init_instance::initialize_sockets() {
 
     if (sd.type != DATA_SOCKET) continue;
 
-    map<sock_dscr, bool>::iterator i = in_done.find(sd);
+    map<sock_dscr, bool>::iterator j = in_done.find(sd);
 
-    if (i != in_done.end()) {
+    if (j != in_done.end()) {
     
       // connection is both in the list of 'out connections' 
       // and in the list of 'in connections'
